48705d18cb00704886bc3753c2a5d23f8268a404
Timothy Cricchio <cricchio.timothy@gmail.com>
2023-02-13 17:39:50 -0500

    Added temporary fix to enemyList before it gets restyled

--- HXUI/helpers.lua
+++ HXUI/helpers.lua
@@ -76,6 +76,52 @@ function GetColorOfTarget(targetEntity, targetIndex)
 	return color;
 end
 
+function GetColorOfTargetRGBA(targetEntity, targetIndex)
+    -- Obtain the entity spawn flags..
+
+	local color = {1,1,1,1};
+	if (targetIndex == nil) then
+		return color;
+	end
+    local flag = targetEntity.SpawnFlags;
+
+    -- Determine the entity type and apply the proper color
+    if (bit.band(flag, 0x0001) == 0x0001) then --players
+		local party = AshitaCore:GetMemoryManager():GetParty();
+		for i = 0, 17 do
+			if (party:GetMemberIsActive(i) == 1) then
+				if (party:GetMemberTargetIndex(i) == targetIndex) then
+					color = {0,1,1,1};
+					break;
+				end
+			end
+		end
+    elseif (bit.band(flag, 0x0002) == 0x0002) then --npc
+        color = {.4,1,.4,1};
+    else --mob
+		local entMgr = AshitaCore:GetMemoryManager():GetEntity();
+		local claimStatus = entMgr:GetClaimStatus(targetIndex);
+		local claimId = bit.band(claimStatus, 0xFFFF);
+--		local isClaimed = (bit.band(claimStatus, 0xFFFF0000) ~= 0);
+
+		if (claimId == 0) then
+			color = {1,1,.4,1};
+		else
+			color = {1,.4,1,1};
+			local party = AshitaCore:GetMemoryManager():GetParty();
+			for i = 0, 17 do
+				if (party:GetMemberIsActive(i) == 1) then
+					if (party:GetMemberServerId(i) == claimId) then
+						color = {1,.4,.4,1};
+						break;
+					end;
+				end
+			end
+		end
+	end
+	return color;
+end
+
 function GetIsMob(targetEntity)
 	if (targetEntity == nil) then
 		return false;
