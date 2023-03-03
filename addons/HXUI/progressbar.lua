1324fba54c764c1e62b7671402f60a568cca2767
Timothy Cricchio <cricchio.timothy@gmail.com>
2023-03-02 23:41:45 -0500

    Restructured hierarchy, implemented statuslib
    Added a submodule that contains all the code to handle collection of status effects and loading of said icons. Removed now unneeded status effect code.
    Also restructured hierarchy to make install simpler for users

---
+++ addons/HXUI/progressbar.lua
@@ -0,0 +1,292 @@
+require('common');
+require('libs/bitmap');
+require('libs/color');
+local imgui = require('imgui');
+local ffi = require('ffi');
+local d3d = require('d3d8');
+local d3d8dev = d3d.get_device();
+
+local progressbar = {
+	-- Bookend
+	bookendFilename = 'bookend',
+
+	-- Background
+	backgroundGradientStartColor = '#01122b',
+	backgroundGradientEndColor = '#061c39',
+	backgroundRounding = 0,
+
+	-- Foreground
+	foregroundRounding = 3,
+	foregroundPadding = 5,
+
+	gradientTextures = {}
+};
+
+function MakeGradientBitmap(startColor, endColor)
+	local height = 100;
+
+	local image = bitmap:new(1, height);
+
+	startColor = table.pack(hex2rgb(startColor));
+	endColor = table.pack(hex2rgb(endColor));
+
+	for pixel = 1, height  do
+		local red = startColor[1] + (endColor[1] - startColor[1]) * (pixel / height);
+		local green = startColor[2] + (endColor[2] - startColor[2]) * (pixel / height);
+		local blue = startColor[3] + (endColor[3] - startColor[3]) * (pixel / height);
+
+		image:setPixelColor(1, (height - pixel) + 1, {red, green, blue, 255});
+	end
+
+	return image;
+end
+
+function GetGradient(startColor, endColor)
+	local texture;
+
+	for i, existingTexture in ipairs(progressbar.gradientTextures) do
+		if (existingTexture.startColor == startColor and existingTexture.endColor == endColor) then
+			texture = existingTexture;
+			break;
+		end
+	end
+
+	if not texture then
+		local image = MakeGradientBitmap(startColor, endColor);
+
+    local texture_ptr = ffi.new('IDirect3DTexture8*[1]');
+
+    local res = ffi.C.D3DXCreateTextureFromFileInMemory(d3d8dev, image:binary(), #image:binary(), texture_ptr);
+
+    if (res ~= ffi.C.S_OK) then
+        error(('%08X (%s)'):fmt(res, d3d.get_error(res)));
+    end
+
+    texture = {
+    	startColor = startColor,
+    	endColor = endColor,
+    	texture = ffi.new('IDirect3DTexture8*', texture_ptr[0])
+    }
+
+    d3d.gc_safe_release(texture.texture);
+
+    table.insert(progressbar.gradientTextures, texture);
+	end
+
+	return tonumber(ffi.cast("uint32_t", texture.texture));
+end
+
+function GetBookendTexture()
+	if not progressbar.bookendTexture then
+		progressbar.bookendTexture = LoadTexture(progressbar.bookendFilename).image;
+	end
+
+	return tonumber(ffi.cast("uint32_t", progressbar.bookendTexture));
+end
+
+progressbar.DrawBar = function(startPosition, endPosition, gradientStart, gradientEnd, rounding, cornerFlags)
+	if not rounding then
+		rounding = 0;
+	end
+
+	local gradient = GetGradient(gradientStart, gradientEnd);
+
+	imgui.GetWindowDrawList():AddImageRounded(gradient, startPosition, endPosition, {0, 0}, {1, 1}, IM_COL32_WHITE, rounding, cornerFlags);
+end
+
+progressbar.DrawColoredBar = function(startPosition, endPosition, color, rounding, cornerFlags)
+	if not rounding then
+		rounding = 0;
+	end
+
+	imgui.GetWindowDrawList():AddRectFilled(startPosition, endPosition, color, rounding, cornerFlags);
+end
+
+progressbar.DrawBookends = function(positionStartX, positionStartY, width, height)
+	local bookendTexture = GetBookendTexture();
+	
+	local bookendWidth = height / 2;
+	
+	-- Draw the left bookend
+	imgui.GetWindowDrawList():AddImage(bookendTexture, {positionStartX, positionStartY}, {positionStartX + bookendWidth, positionStartY + height}, {0, 0}, {1, 1}, IM_COL32_WHITE);
+	
+	-- Draw the right bookend
+	imgui.GetWindowDrawList():AddImage(bookendTexture, {positionStartX + width - bookendWidth, positionStartY}, {positionStartX + width, positionStartY + height}, {1, 1}, {0, 0}, IM_COL32_WHITE);
+end
+
+progressbar.ProgressBar  = function(percentList, dimensions, options)
+	if options == nil then
+		options = {};
+	end
+
+	-- Decorate by default
+	if options.decorate == nil then
+		options.decorate = true;
+	end
+	
+	local positionStartX, positionStartY = imgui.GetCursorScreenPos();
+	
+	local width = dimensions[1];
+	local height = dimensions[2];
+	
+	-- If our width is 0 or less, we instead get the content region's available space
+	-- which allows us to stretch the progress bar to fit the content region.
+	if width <= 0 then
+		width = math.floor(imgui.GetContentRegionAvail() + 0.5);
+	end
+	
+	local contentWidth = width;
+	local contentPositionStartX = positionStartX;
+	local contentPositionStartY = positionStartY;
+	local rounding;
+
+	-- Draw the bookends!
+	if options.decorate then
+		local bookendWidth = height / 2;
+		
+		contentWidth = width - (bookendWidth * 2);
+		contentPositionStartX = contentPositionStartX + bookendWidth;
+		
+		progressbar.DrawBookends(positionStartX, positionStartY, width, height);
+	end
+	
+	-- Draw the background
+	local bgGradientStart = progressbar.backgroundGradientStartColor;
+	local bgGradientEnd = progressbar.backgroundGradientEndColor;
+
+	if options.backgroundGradientOverride then
+		bgGradientStart = options.backgroundGradientOverride[1];
+		bgGradientEnd = options.backgroundGradientOverride[2];
+	end
+
+	rounding = options.decorate and progressbar.backgroundRounding or gConfig.noBookendRounding;
+	progressbar.DrawBar({contentPositionStartX, contentPositionStartY}, {contentPositionStartX + contentWidth, contentPositionStartY + height}, bgGradientStart, bgGradientEnd, rounding);
+	
+	-- Compute the actual progress bar's width and height
+	local paddingHalf = progressbar.foregroundPadding / 2;
+	
+	local progressPositionStartX = contentPositionStartX + paddingHalf;
+	local progressPositionStartY = contentPositionStartY + paddingHalf;
+	
+	local progressTotalWidth = contentWidth - progressbar.foregroundPadding;
+	local progressHeight = height - progressbar.foregroundPadding;
+	
+	-- Draw the progress bar(s)
+	local progressOffset = 0;
+	
+	for i, percentData in ipairs(percentList) do
+		local percent = math.clamp(percentData[1], 0, 1);
+
+		local cornerFlags = ImDrawCornerFlags_All;
+
+		if #percentList > 1 then
+			if i == 1 then
+				cornerFlags = ImDrawCornerFlags_Left;
+			elseif i == #percentList then
+				cornerFlags = ImDrawCornerFlags_Right;
+			else
+				cornerFlags = ImDrawCornerFlags_None;
+			end
+		end
+		
+		if percent > 0 then
+			local startColor = percentData[2][1];
+			local endColor = percentData[2][2];
+			local overlayConfiguration = percentData[3];
+			
+			local progressWidth = progressTotalWidth * percent;
+			
+			rounding = options.decorate and progressbar.foregroundRounding or gConfig.noBookendRounding;
+			progressbar.DrawBar({progressPositionStartX + progressOffset, progressPositionStartY}, {progressPositionStartX + progressOffset + progressWidth, progressPositionStartY + progressHeight}, startColor, endColor, rounding, cornerFlags);
+
+			if overlayConfiguration then
+				local overlayColor = overlayConfiguration[1];
+				local overlayAlpha = overlayConfiguration[2];
+				-- local overlayPercent = overlayConfiguration[3];
+
+				local overlayWidth = progressWidth;
+				local overlayCornerFlags = cornerFlags;
+
+				--[[
+				if overlayPercent then
+					overlayWidth = progressTotalWidth * overlayPercent;
+					overlayCornerFlags = ImDrawCornerFlags_None;
+				end
+				]]--
+
+				local red, green, blue = hex2rgb(overlayColor);
+
+				local overlayBarColor = imgui.GetColorU32({red / 255, green / 255, blue / 255, overlayAlpha});
+
+				rounding = options.decorate and progressbar.foregroundRounding or gConfig.noBookendRounding;
+				progressbar.DrawColoredBar({progressPositionStartX + progressOffset, progressPositionStartY}, {progressPositionStartX + progressOffset + overlayWidth, progressPositionStartY + progressHeight}, overlayBarColor, rounding, cornerFlags);
+			end
+			
+			progressOffset = progressOffset + progressWidth;
+		end
+	end
+
+	-- Draw the optional overlay bar (used for TP)
+	if options.overlayBar then
+		local overlayPercent = options.overlayBar[1][1];
+		local overlayGradientStart = options.overlayBar[1][2][1];
+		local overlayGradientEnd = options.overlayBar[1][2][2];
+		local overlayHeight = options.overlayBar[2];
+		local overlayTopPadding = options.overlayBar[3];
+
+		local overlayWidth = progressTotalWidth;
+
+		-- Draw the overlay background
+		rounding = options.decorate and progressbar.backgroundRounding or gConfig.noBookendRounding;
+		progressbar.DrawBar({progressPositionStartX, progressPositionStartY + progressHeight - overlayHeight}, {progressPositionStartX + overlayWidth, progressPositionStartY + progressHeight}, progressbar.backgroundGradientStartColor, progressbar.backgroundGradientEndColor, rounding);
+
+		-- Draw the overlay progress bar
+		local overlayProgressWidth = overlayWidth * overlayPercent;
+
+		rounding = options.decorate and progressbar.foregroundRounding or gConfig.noBookendRounding;
+		progressbar.DrawBar({progressPositionStartX, progressPositionStartY + progressHeight - overlayHeight + overlayTopPadding}, {progressPositionStartX + overlayProgressWidth, progressPositionStartY + progressHeight}, overlayGradientStart, overlayGradientEnd, rounding);
+
+		-- Allow optional pulsing of overlay bars
+		local pulseConfiguration = options.overlayBar[4];
+
+		if pulseConfiguration then
+			local currentTime = os.clock();
+			local timePerPulse = pulseConfiguration[2];
+			local phase = currentTime % timePerPulse;
+			local pulseAlpha = (2 / timePerPulse) * phase;
+
+			if pulseAlpha > 1 then
+				pulseAlpha = 2 - pulseAlpha;
+			end
+
+			local pulseColor = pulseConfiguration[1];
+			local red, green, blue = hex2rgb(pulseColor);
+
+			local pulseBarColor = imgui.GetColorU32({red / 255, green / 255, blue / 255, pulseAlpha});
+
+			rounding = options.decorate and progressbar.foregroundRounding or gConfig.noBookendRounding;
+			progressbar.DrawColoredBar({progressPositionStartX, progressPositionStartY + progressHeight - overlayHeight + overlayTopPadding}, {progressPositionStartX + overlayProgressWidth, progressPositionStartY + progressHeight}, pulseBarColor, rounding);
+		end
+	end
+
+	if options.borderConfig then
+		local borderWidth = options.borderConfig[1];
+		local borderColorRed, borderColorGreen, borderColorBlue = hex2rgb(options.borderConfig[2]);
+		rounding = options.decorate and height/2 or gConfig.noBookendRounding;
+		imgui.GetWindowDrawList():AddRect({positionStartX - (borderWidth / 2), positionStartY - (borderWidth / 2)}, {positionStartX + width + (borderWidth / 2), positionStartY + height + (borderWidth / 2)}, imgui.GetColorU32({borderColorRed / 255, borderColorGreen / 255, borderColorBlue / 255, 1}), rounding, ImDrawCornerFlags_All, borderWidth);
+	end
+	
+	svgrenderer.dropShadow(
+		{positionStartX, positionStartY},
+		{positionStartX + width, positionStartY + height},
+		rounding,
+		0, -- offset X
+		2, -- offset Y
+		3, -- blur radius
+		0.4 -- alpha
+	);
+	
+	imgui.Dummy({width, height});
+end
+
+return progressbar;
---
+++ addons/HXUI/targetbar.lua
@@ -0,0 +1,415 @@
+require('common');
+require('helpers');
+local imgui = require('imgui');
+local assetLoader = require('assetLoader');
+local progressbar = require('progressbar');
+local fonts = require('fonts');
+local ffi = require("ffi");
+
+local bgAlpha = 0.4;
+local bgRadius = 3;
+
+local arrowTexture;
+
+local targetbar = {
+	interpolation = {}
+};
+
+local _HXUI_DEV_DEBUG_INTERPOLATION = falsed;
+local _HXUI_DEV_DEBUG_INTERPOLATION_DELAY = 1;
+local _HXUI_DEV_DEBUG_HP_PERCENT_PERSISTENT = 100;
+local _HXUI_DEV_DAMAGE_SET_TIMES = {};
+
+targetbar.drawStatusIcons = function(player, playerEnt, playerTarget, targetEntity, targetIndex)
+
+	local statusIds = gStatusLib.GetStatusIdsByIndex(targetIndex);
+	if not statusIds then
+		return;
+	end
+
+	local buffIds = {};
+	local debuffIds = {};
+
+	for i = 1, #statusIds do
+		if statusIds[i] ~= -1 then
+			if gStatusLib.GetIsBuff(statusIds[i]) then
+				table.insert(buffIds, statusIds[i]);
+			else
+				gStatusLib.insert(debuffIds, statusIds[i]);
+			end
+		end
+	end
+
+	-- You can uncomment these for debug purposes, just make sure you comment out the above logic
+	-- local buffIds = {253, 445, 198, 199};
+	-- local debuffIds = {2, 6};
+
+	local bgSize = {24, 28};
+	local iconSize = {18, 18};
+
+	local buffTopPadding = 7;
+	local buffLeftPadding = 3;
+
+	imgui.PushStyleVar(ImGuiStyleVar_ItemSpacing, {4, imgui.GetStyle().ItemSpacing.y});
+
+	-- ==========
+	-- Draw buffs
+	-- ==========
+	for i, buffId in ipairs(buffIds) do
+		if i == 1 then
+			imgui.SetCursorPosX(imgui.GetCursorPosX() + 10);
+		end
+
+		local iconStartX, iconStartY  = imgui.GetCursorScreenPos();
+
+		local bgTexture = assetLoader.GetBackground(true);
+
+		local iconTexture = gStatusLib.GetIconForStatusId(buffId, gConfig.statusIconTheme);
+
+		imgui.Image(bgTexture, bgSize);
+
+		imgui.GetWindowDrawList():AddImage(
+			iconTexture,
+			{
+				iconStartX + buffLeftPadding,
+				iconStartY + buffTopPadding;
+			},
+			{
+				iconStartX + buffLeftPadding + iconSize[1],
+				iconStartY + buffTopPadding + iconSize[2] 
+			},
+			{0, 0},
+			{1, 1},
+			IM_COL32_WHITE
+		);
+
+		if i < #buffIds then
+			imgui.SameLine();
+		end
+	end
+
+	local debuffTopPadding = 3;
+	local debuffLeftPadding = 3;
+
+	-- ==========
+	-- Draw debuffs
+	-- ==========
+	for i, debuffId in ipairs(debuffIds) do
+		if i == 1 then
+			imgui.SetCursorPosX(imgui.GetCursorPosX() + 10);
+		end
+
+		local iconStartX, iconStartY = imgui.GetCursorScreenPos();
+
+		local bgTexture = assetLoader.GetBackground(false);
+
+		local iconTexture = gStatusLib.GetIconForStatusId(debuffId, gConfig.statusIconTheme);
+
+		imgui.Image(bgTexture, bgSize);
+
+		imgui.GetWindowDrawList():AddImage(
+			iconTexture,
+			{
+				iconStartX + debuffLeftPadding,
+				iconStartY + debuffTopPadding;
+			},
+			{
+				iconStartX + debuffLeftPadding + iconSize[1],
+				iconStartY + debuffTopPadding + iconSize[2] 
+			},
+			{0, 0},
+			{1, 1},
+			IM_COL32_WHITE
+		);
+
+		if i < #debuffIds  then
+			imgui.SameLine();
+		end
+	end
+
+	imgui.PopStyleVar(1);
+end
+
+targetbar.DrawWindow = function(settings)
+    -- Obtain the player entity..
+    local playerEnt = GetPlayerEntity();
+	local player = AshitaCore:GetMemoryManager():GetPlayer();
+
+    if (playerEnt == nil or player == nil) then
+        return;
+    end
+
+    -- Obtain the player target entity (account for subtarget)
+	local playerTarget = AshitaCore:GetMemoryManager():GetTarget();
+
+	local targetIndex;
+	local targetEntity;
+	
+	if (playerTarget ~= nil) then
+		targetIndex, _ = gStatusLib.GetTargets();
+		targetEntity = GetEntity(targetIndex);
+	end
+
+    if (targetEntity == nil or targetEntity.Name == nil) then
+		targetbar.interpolation.interpolationDamagePercent = 0;
+
+        return;
+    end
+
+	local currentTime = os.clock();
+
+	local hppPercent = targetEntity.HPPercent;
+
+	-- Mimic damage taken
+	if _HXUI_DEV_DEBUG_INTERPOLATION then
+		if _HXUI_DEV_DAMAGE_SET_TIMES[1] and currentTime > _HXUI_DEV_DAMAGE_SET_TIMES[1][1] then
+			_HXUI_DEV_DEBUG_HP_PERCENT_PERSISTENT = _HXUI_DEV_DAMAGE_SET_TIMES[1][2];
+
+			table.remove(_HXUI_DEV_DAMAGE_SET_TIMES, 1);
+		end
+
+		if #_HXUI_DEV_DAMAGE_SET_TIMES == 0 then
+			local previousHitTime = currentTime + 1;
+			local previousHp = 100;
+
+			local totalDamageInstances = 10;
+
+			for i = 1, totalDamageInstances do
+				local hitDelay = math.random(0.25 * 100, 1.25 * 100) / 100;
+				local damageAmount = math.random(1, 20);
+
+				if i > 1 and i < totalDamageInstances then
+					previousHp = math.max(previousHp - damageAmount, 0);
+				end
+
+				if i < totalDamageInstances then
+					previousHitTime = previousHitTime + hitDelay;
+				else
+					previousHitTime = previousHitTime + _HXUI_DEV_DEBUG_INTERPOLATION_DELAY;
+				end
+
+				_HXUI_DEV_DAMAGE_SET_TIMES[i] = {previousHitTime, previousHp};
+			end
+		end
+
+		hppPercent = _HXUI_DEV_DEBUG_HP_PERCENT_PERSISTENT;
+	end
+
+	-- If we change targets, reset the interpolation
+	if targetbar.interpolation.currentTargetId ~= targetIndex then
+		targetbar.interpolation.currentTargetId = targetIndex;
+		targetbar.interpolation.currentHpp = hppPercent;
+		targetbar.interpolation.interpolationDamagePercent = 0;
+	end
+
+	-- If the target takes damage
+	if hppPercent < targetbar.interpolation.currentHpp then
+		local previousInterpolationDamagePercent = targetbar.interpolation.interpolationDamagePercent;
+
+		local damageAmount = targetbar.interpolation.currentHpp - hppPercent;
+
+		targetbar.interpolation.interpolationDamagePercent = targetbar.interpolation.interpolationDamagePercent + damageAmount;
+
+		if previousInterpolationDamagePercent > 0 and targetbar.interpolation.lastHitAmount and damageAmount > targetbar.interpolation.lastHitAmount then
+			targetbar.interpolation.lastHitTime = currentTime;
+			targetbar.interpolation.lastHitAmount = damageAmount;
+		elseif previousInterpolationDamagePercent == 0 then
+			targetbar.interpolation.lastHitTime = currentTime;
+			targetbar.interpolation.lastHitAmount = damageAmount;
+		end
+
+		if not targetbar.interpolation.lastHitTime or currentTime > targetbar.interpolation.lastHitTime + (settings.hitFlashDuration * 0.25) then
+			targetbar.interpolation.lastHitTime = currentTime;
+			targetbar.interpolation.lastHitAmount = damageAmount;
+		end
+
+		-- If we previously were interpolating with an empty bar, reset the hit delay effect
+		if previousInterpolationDamagePercent == 0 then
+			targetbar.interpolation.hitDelayStartTime = currentTime;
+		end
+	elseif hppPercent > targetbar.interpolation.currentHpp then
+		-- If the target heals
+		targetbar.interpolation.interpolationDamagePercent = 0;
+		targetbar.interpolation.hitDelayStartTime = nil;
+	end
+
+	targetbar.interpolation.currentHpp = hppPercent;
+
+	-- Reduce the HP amount to display based on the time passed since last frame
+	if targetbar.interpolation.interpolationDamagePercent > 0 and targetbar.interpolation.hitDelayStartTime and currentTime > targetbar.interpolation.hitDelayStartTime + settings.hitDelayDuration then
+		if targetbar.interpolation.lastFrameTime then
+			local deltaTime = currentTime - targetbar.interpolation.lastFrameTime;
+
+			local animSpeed = 0.1 + (0.9 * (targetbar.interpolation.interpolationDamagePercent / 100));
+
+			-- animSpeed = math.max(settings.hitDelayMinAnimSpeed, animSpeed);
+
+			targetbar.interpolation.interpolationDamagePercent = targetbar.interpolation.interpolationDamagePercent - (settings.hitInterpolationDecayPercentPerSecond * deltaTime * animSpeed);
+
+			-- Clamp our percent to 0
+			targetbar.interpolation.interpolationDamagePercent = math.max(0, targetbar.interpolation.interpolationDamagePercent);
+		end
+	end
+
+	if gConfig.healthBarFlashEnabled then
+		if targetbar.interpolation.lastHitTime and currentTime < targetbar.interpolation.lastHitTime + settings.hitFlashDuration then
+			local hitFlashTime = currentTime - targetbar.interpolation.lastHitTime;
+			local hitFlashTimePercent = hitFlashTime / settings.hitFlashDuration;
+
+			local maxAlphaHitPercent = 20;
+			local maxAlpha = math.min(targetbar.interpolation.lastHitAmount, maxAlphaHitPercent) / maxAlphaHitPercent;
+
+			maxAlpha = math.max(maxAlpha * 0.6, 0.4);
+
+			targetbar.interpolation.overlayAlpha = math.pow(1 - hitFlashTimePercent, 2) * maxAlpha;
+		end
+	end
+
+	targetbar.interpolation.lastFrameTime = currentTime;
+
+	local color = GetColorOfTarget(targetEntity, targetIndex);
+	local isMonster = GetIsMob(targetEntity);
+
+	-- Draw the main target window
+	local windowFlags = bit.bor(ImGuiWindowFlags_NoDecoration, ImGuiWindowFlags_AlwaysAutoResize, ImGuiWindowFlags_NoFocusOnAppearing, ImGuiWindowFlags_NoNav, ImGuiWindowFlags_NoBringToFrontOnFocus);
+
+	windowFlags = bit.bor(windowFlags, ImGuiWindowFlags_NoBackground);
+
+	if (gConfig.lockPositions) then
+		windowFlags = bit.bor(windowFlags, ImGuiWindowFlags_NoMove);
+	end
+
+	imgui.SetNextWindowSize({settings.barWidth, -1});
+
+	local mainWindowPosX, mainWindowPosY, mainWindowWidth;
+
+    if (imgui.Begin('TargetBar', true, windowFlags)) then
+		-- Obtain and prepare target information..
+        local dist  = ('%.1f'):fmt(math.sqrt(targetEntity.Distance));
+		local targetNameText = targetEntity.Name;
+		local targetHpPercent = targetEntity.HPPercent..'%';
+
+		if (gConfig.showEnemyId and isMonster) then
+			local targetServerId = AshitaCore:GetMemoryManager():GetEntity():GetServerId(targetIndex);
+			local targetServerIdHex = string.format('0x%X', targetServerId);
+
+			targetNameText = targetNameText .. " [".. string.sub(targetServerIdHex, -3) .."]";
+		end
+
+		svgrenderer.text('targetbar_name', targetNameText, 18, getTargetGradient(targetEntity, targetIndex), {marginX=10});
+
+		imgui.SameLine();
+
+		svgrenderer.text('targetbar_dist', dist .. 'Y', 18, HXUI_COL_WHITE, {justify='right', marginX=10});
+
+		local hpGradientStart = '#e26c6c';
+		local hpGradientEnd = '#fb9494';
+
+		local hpPercentData = {{targetEntity.HPPercent / 100, {hpGradientStart, hpGradientEnd}}};
+
+		if _HXUI_DEV_DEBUG_INTERPOLATION then
+			hpPercentData[1][1] = targetbar.interpolation.currentHpp / 100;
+		end
+
+		if targetbar.interpolation.interpolationDamagePercent > 0 then
+			local interpolationOverlay;
+
+			if gConfig.healthBarFlashEnabled then
+				interpolationOverlay = {
+					'#FFFFFF', -- overlay color,
+					targetbar.interpolation.overlayAlpha -- overlay alpha,
+				};
+			end
+
+			table.insert(
+				hpPercentData,
+				{
+					targetbar.interpolation.interpolationDamagePercent / 100, -- interpolation percent
+					{'#cf3437', '#c54d4d'},
+					interpolationOverlay
+				}
+			);
+		end
+		
+		progressbar.ProgressBar(hpPercentData, {-1, settings.barHeight}, {decorate = gConfig.showTargetBarBookends});
+
+		if isMonster or gConfig.alwaysShowHealthPercent then
+			local currentCursorPosX = imgui.GetCursorPosX();
+
+			imgui.SetCursorPosY(imgui.GetCursorPosY() - 15);
+
+			svgrenderer.text('targetbar_hpp', string.format('%d%%', targetEntity.HPPercent), 16, HXUI_COL_WHITE, {justify='right', marginX=16});
+			
+			imgui.SameLine();
+
+			imgui.SetCursorPosY(imgui.GetCursorPosY() + 15);
+
+			imgui.SetCursorPosX(currentCursorPosX);
+		end
+
+		targetbar.drawStatusIcons(player, playerEnt, playerTarget, targetEntity, targetIndex);
+
+		mainWindowPosX, mainWindowPosY = imgui.GetWindowPos();
+		mainWindowWidth = imgui.GetWindowWidth();
+    end
+    
+	imgui.End();
+
+	local totEntity;
+	local totIndex;
+
+	if (targetEntity == playerEnt) then
+		totIndex = targetIndex
+		totEntity = targetEntity;
+	end
+
+	if (totEntity == nil) then
+		totIndex = targetEntity.TargetedIndex;
+		if (totIndex ~= nil) then
+			totEntity = GetEntity(totIndex);
+		end
+	end
+
+	if (totEntity ~= nil and totEntity.Name ~= nil) then
+		local totWindowFlags = bit.bor(ImGuiWindowFlags_NoMove, ImGuiWindowFlags_NoDecoration, ImGuiWindowFlags_AlwaysAutoResize, ImGuiWindowFlags_NoFocusOnAppearing, ImGuiWindowFlags_NoNav, ImGuiWindowFlags_NoBringToFrontOnFocus);
+
+		totWindowFlags = bit.bor(totWindowFlags, ImGuiWindowFlags_NoBackground);
+
+		imgui.SetNextWindowSize({settings.barWidth / 2, -1});
+
+		imgui.SetNextWindowPos({mainWindowPosX + mainWindowWidth, mainWindowPosY});
+
+		if (imgui.Begin('TargetOfTargetBar', true, totWindowFlags)) then
+			imgui.BeginGroup();
+
+			imgui.SetCursorPosY(imgui.GetCursorPosY() + 30);
+
+			imgui.Image(tonumber(ffi.cast("uint32_t", arrowTexture.image)), {24, 24});
+
+			imgui.EndGroup();
+
+			imgui.SameLine();
+
+			imgui.BeginGroup();
+
+			svgrenderer.text('targetbar_name', totEntity.Name, 18, getTargetGradient(targetEntity, targetIndex), {marginX=10});
+
+			local hpGradientStart = '#e26c6c';
+			local hpGradientEnd = '#fb9494';
+
+			local hpPercentData = {{totEntity.HPPercent / 100, {hpGradientStart, hpGradientEnd}}};
+
+			progressbar.ProgressBar(hpPercentData, {-1, settings.barHeight}, {decorate = gConfig.showTargetBarBookends});
+
+			imgui.EndGroup();
+		end
+
+		imgui.End();
+	end
+end
+
+targetbar.Initialize = function(settings)
+	arrowTexture = LoadTexture("arrow");
+end
+
+return targetbar;
