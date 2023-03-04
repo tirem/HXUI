0b9b910f4f1867e2fbcbd20acb8c7413591f7276
Timothy Cricchio <cricchio.timothy@gmail.com>
2023-03-03 21:57:13 -0500

    Updated login check code

--- addons/HXUI/HXUI.lua
+++ addons/HXUI/HXUI.lua
@@ -723,86 +723,13 @@ settings.register('settings', 'settings_update', function (s)
     end
 end);
 
--- Get if we are logged in right when the addon loads
-bLoggedIn = false;
-local playerIndex = AshitaCore:GetMemoryManager():GetParty():GetMemberTargetIndex(0);
-if playerIndex ~= 0 then
-    local entity = AshitaCore:GetMemoryManager():GetEntity();
-    local flags = entity:GetRenderFlags0(playerIndex);
-    if (bit.band(flags, 0x200) == 0x200) and (bit.band(flags, 0x4000) == 0) then
-        bLoggedIn = true;
-	end
-end
-
---Thanks to Velyn for the event system and interface hidden signatures!
-local pGameMenu = ashita.memory.find('FFXiMain.dll', 0, "8B480C85C974??8B510885D274??3B05", 16, 0);
-local pEventSystem = ashita.memory.find('FFXiMain.dll', 0, "A0????????84C0741AA1????????85C0741166A1????????663B05????????0F94C0C3", 0, 0);
-local pInterfaceHidden = ashita.memory.find('FFXiMain.dll', 0, "8B4424046A016A0050B9????????E8????????F6D81BC040C3", 0, 0);
-
-local function GetMenuName()
-    local subPointer = ashita.memory.read_uint32(pGameMenu);
-    local subValue = ashita.memory.read_uint32(subPointer);
-    if (subValue == 0) then
-        return '';
-    end
-    local menuHeader = ashita.memory.read_uint32(subValue + 4);
-    local menuName = ashita.memory.read_string(menuHeader + 0x46, 16);
-    return string.gsub(menuName, '\x00', '');
-end
-
-local function GetEventSystemActive()
-    if (pEventSystem == 0) then
-        return false;
-    end
-    local ptr = ashita.memory.read_uint32(pEventSystem + 1);
-    if (ptr == 0) then
-        return false;
-    end
-
-    return (ashita.memory.read_uint8(ptr) == 1);
-
-end
-
-local function GetInterfaceHidden()
-    if (pEventSystem == 0) then
-        return false;
-    end
-    local ptr = ashita.memory.read_uint32(pInterfaceHidden + 10);
-    if (ptr == 0) then
-        return false;
-    end
-
-    return (ashita.memory.read_uint8(ptr + 0xB4) == 1);
-end
-
-function GetHidden()
-
-	if (GetEventSystemActive()) then
-		return true;
-	end
-
-	if (string.match(GetMenuName(), 'map')) then
-		return true;
-	end
-
-    if (GetInterfaceHidden()) then
-        return true;
-    end
-
-	if (bLoggedIn == false) then
-		return true;
-	end
-    
-    return false;
-end
-
 --[[
 * event: d3d_present
 * desc : Event called when the Direct3D device is presenting a scene.
 --]]
 ashita.events.register('d3d_present', 'present_cb', function ()
 
-	if (GetHidden() == false) then
+	if (gStatusLib.GetGameInterfaceHidden() == false) then
 		if (gConfig.showPlayerBar) then
 			playerBar.DrawWindow(gAdjustedSettings.playerBarSettings);
 		end
@@ -889,8 +816,5 @@ ashita.events.register('packet_in', 'packet_in_cb', function (e)
 		end
 	elseif (e.id == 0x00A) then
 		partyList.HandleZonePacket(e);
-		bLoggedIn = true;
-	elseif (e.id == 0x00B) then
-		bLoggedIn = false;
 	end
 end);
