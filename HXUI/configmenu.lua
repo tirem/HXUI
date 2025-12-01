888ccb5e9df5219c9500a0310c2446da53ed35bb
Tirem <cricchio.timothy@gmail.com>
2025-12-02 08:42:24 +0900

    Gating off target cast bars

--- HXUI/configmenu.lua
+++ HXUI/configmenu.lua
@@ -246,8 +246,10 @@ local function DrawTargetBarSettings()
         DrawCheckbox('Show Bookends', 'showTargetBarBookends');
         DrawCheckbox('Show Lock On', 'showTargetBarLockOnBorder');
         imgui.ShowHelp('Display the lock icon and colored border when locked on to a target.');
-        DrawCheckbox('Show Cast Bar', 'showTargetBarCastBar');
-        imgui.ShowHelp('Display the enemy cast bar under the HP bar when the target is casting.');
+        if (not HorizonLimitedMode) then
+            DrawCheckbox('Show Cast Bar', 'showTargetBarCastBar');
+            imgui.ShowHelp('Display the enemy cast bar under the HP bar when the target is casting.');
+        end
         DrawCheckbox('Hide During Events', 'targetBarHideDuringEvents');
         DrawCheckbox('Show Enemy Id', 'showEnemyId');
         imgui.ShowHelp('Display the internal ID of the monster next to its name.');
@@ -265,7 +267,7 @@ local function DrawTargetBarSettings()
         DrawSlider('HP% Font Size', 'targetBarPercentFontSize', 8, 36);
 
         -- Cast bar settings (only show if cast bar is enabled)
-        if (gConfig.showTargetBarCastBar) then
+        if (gConfig.showTargetBarCastBar and (not HorizonLimitedMode)) then
             DrawSlider('Cast Font Size', 'targetBarCastFontSize', 8, 36);
             imgui.ShowHelp('Font size for enemy cast text that appears under the HP bar.');
 
