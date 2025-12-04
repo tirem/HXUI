6192917261abfd943ca5275a5aa99a1782cb048b
shuu37 <codywo37@gmail.com>
2025-12-04 09:56:29 -0800

    fix release num

--- XIUI/XIUI.lua
+++ XIUI/XIUI.lua
@@ -24,7 +24,7 @@
 
 addon.name      = 'XIUI';
 addon.author    = 'Team XIUI';
-addon.version   = '1.5.0';
+addon.version   = '1.4.3';
 addon.desc      = 'Multiple UI elements with manager';
 addon.link      = 'https://github.com/tirem/XIUI'
 
